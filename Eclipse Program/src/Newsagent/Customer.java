42d1baa6fbeb4a830ae0dd210357f13f0fe7167f
PodgeMc <padraig.a.d.mccormack@outlook.com>
2024-10-14 11:57:45 +0100

    Added Customer Class
    Added customer class with getters and setters for all the variables

--- Eclipse Program/src/Newsagent/Customer.java
+++ Eclipse Program/src/Newsagent/Customer.java
@@ -2,5 +2,60 @@ package Newsagent;
 
 public class Customer
 {
+	public String name;
+	public String address;
+	public int phoneNumber;
+	public String subscriptionStatus;
+	public boolean pauseActive;
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getAddress()
+	{
+		return address;
+	}
+	
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+	
+	public int getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(int phoneNumber)
+	{
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getSubscriptionStatus()
+	{
+		return subscriptionStatus;
+	}
+	
+	public void setSubscriptionStatus(String subscriptionStatus)
+	{
+		this.subscriptionStatus = subscriptionStatus;
+	}
+	
+	public boolean isPauseActive()
+	{
+		return pauseActive;
+	}
+	
+	public void setPauseActive(boolean pauseActive)
+	{
+		this.pauseActive = pauseActive;
+	}
 	
 }
