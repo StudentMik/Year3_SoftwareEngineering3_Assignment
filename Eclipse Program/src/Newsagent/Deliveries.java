e2c87d8919cca59f109d8e79d4d1852f06fb2651
Liam <73363528+Liamm03@users.noreply.github.com>
2024-10-15 10:20:32 +0100

    Update Deliveries.java

--- Eclipse Program/src/Newsagent/Deliveries.java
+++ Eclipse Program/src/Newsagent/Deliveries.java
@@ -2,5 +2,83 @@ package Newsagent;
 
 public class Deliveries
 {
+	private int driverID;
+	private int orderId;
+	private String publicationID;  //publicationsdelivered
+	private String customerName; 
+	private String deliveryAddress; //deliveryarea
+	private String activeDays;  //deliveryDate
+	private boolean deliveryStatus;
+	private boolean deliveriesPaused;
 	
+	public Deliveries(int driverID, int orderId, String publicationID, String customerName, String deliveryAddress,
+			String activeDays, boolean deliveryStatus, boolean deliveriesPaused) {
+		this.driverID = driverID;
+		this.orderId = orderId;
+		this.publicationID = publicationID;
+		this.customerName = customerName;
+		this.deliveryAddress = deliveryAddress;
+		this.activeDays = activeDays;
+		this.deliveryStatus = deliveryStatus;
+		this.deliveriesPaused = deliveriesPaused;
+	}
+	
+	public int getDriverID() {
+		return driverID;
+	}
+	public void setDriverID(int driverID) {
+		this.driverID = driverID;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getPublicationID() {
+		return publicationID;
+	}
+	public void setPublicationID(String publicationID) {
+		this.publicationID = publicationID;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getDeliveryAddress() {
+		return deliveryAddress;
+	}
+	public void setDeliveryAddress(String deliveryAddress) {
+		this.deliveryAddress = deliveryAddress;
+	}
+
+	public String getActiveDays() {
+		return activeDays;
+	}
+	public void setActiveDays(String activeDays) {
+		this.activeDays = activeDays;
+	}
+
+	public boolean isDeliveryStatus() {
+		return deliveryStatus;
+	}
+	public void setDeliveryStatus(boolean deliveryStatus) {
+		this.deliveryStatus = deliveryStatus;
+	}
+	public boolean isDeliveriesPaused() {
+		return deliveriesPaused;
+	}
+	public void setDeliveriesPaused(boolean deliveriesPaused) {
+		this.deliveriesPaused = deliveriesPaused;
+	}
+
+	public static void main(String args[]) {
+		
+	}
 }
