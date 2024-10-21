7ab61299c85767497d198267c082b4cc44c0d171
Liam <73363528+Liamm03@users.noreply.github.com>
2024-10-21 12:51:20 +0100

    Create InvoiceExceptionHandler.java

---
+++ Test Design/Junit Testing/Invoice/InvoiceExceptionHandler.java
@@ -0,0 +1,14 @@
+package Newsagent;
+
+public class InvoiceExceptionHandler extends Exception {
+
+	String message;
+	
+	public InvoiceExceptionHandler(String errMessage) {
+		message = errMessage;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+}
