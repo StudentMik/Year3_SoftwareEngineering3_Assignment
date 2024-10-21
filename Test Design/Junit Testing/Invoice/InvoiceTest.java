7565af6859adedbe78199ff8147812ff6ca49871
Liam <73363528+Liamm03@users.noreply.github.com>
2024-10-21 12:52:20 +0100

    Add files via upload

---
+++ Test Design/Junit Testing/Invoice/InvoiceTest.java
@@ -0,0 +1,10 @@
+package Newsagent;
+import junit.framework.TestCase;
+
+public class InvoiceTest extends TestCase {
+
+	// Test 1
+	// Objective: Validate Invoice Creation
+	// Input: customerID =
+	// Expected Output: 
+}
