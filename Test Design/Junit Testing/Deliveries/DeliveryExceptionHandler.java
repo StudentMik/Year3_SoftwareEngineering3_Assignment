5d1e3898dcd76b45d1ffaeeebbda74a8f031078b
Liam <73363528+Liamm03@users.noreply.github.com>
2024-10-21 12:53:10 +0100

    Create DeliveryExceptionHandler.java

---
+++ Test Design/Junit Testing/Deliveries/DeliveryExceptionHandler.java
@@ -0,0 +1,15 @@
+package Newsagent;
+
+public class DeliveryExceptionHandler extends Exception {
+	
+	String message;
+	
+	public DeliveryExceptionHandler(String errMessage) {
+		message = errMessage;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+}
