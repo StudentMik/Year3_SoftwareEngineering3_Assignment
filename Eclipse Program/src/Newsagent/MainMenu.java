9309ba373b89925c02239369f2abd74bf0a27e54
Liam <73363528+Liamm03@users.noreply.github.com>
2024-11-05 10:17:15 +0000

    Added and Tested sample MainMenu
    Working so far, may need to change in the future

---
+++ Eclipse Program/src/Newsagent/MainMenu.java
@@ -0,0 +1,62 @@
+package Newsagent;
+
+import java.util.Scanner;
+
+public class MainMenu {
+
+	
+	public static void main(String[] args) throws DeliveryExceptionHandler, InvoiceExceptionHandler, PublicationExceptionHandler
+	{
+		Scanner in = new Scanner(System.in);
+		 System.out.println("Choose an Option:");
+	        System.out.println("1.	Customer");
+	       	System.out.println("2.	Deliveries");
+	       	System.out.println("3.	Invoice");
+	       	System.out.println("4.	Order ");
+	       	System.out.println("5.	Publications");
+	       	System.out.println("6.	Exit");
+	       	System.out.println(":	");
+	       	
+	       	int choice = in.nextInt();
+	       	in.nextLine();
+
+	        switch (choice) 
+	       	{
+	       		case 1:
+	       			Customer.main(null);
+	       			in.close();
+	       			break;
+
+	       		case 2:
+	       			Deliveries.main(null);
+	       			in.close();
+	       			break;
+	       			
+	       		case 3:
+	       			Invoice.main(null);
+	       			in.close();
+	       			break;
+	       			
+	       		case 4:
+	       			Order.main(null);
+	       			in.close();
+	       			break;
+	       			
+	       		case 5:
+	       			Publications.main(null);
+	       			in.close();
+	       			break;
+
+	       		case 6:
+	       			System.out.println("Exiting.....");
+	       			in.close();
+	       			return;
+	        				
+	       		default:
+	       			System.out.println("Error: Invalid Choice");
+	       			in.close();
+	        }
+		}
+		
+	}
+
