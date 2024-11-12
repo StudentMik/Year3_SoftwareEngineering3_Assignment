d4e5c0d8d8ce027a5bc98ec0b47a84588eea6018
PodgeMc <padraig.a.d.mccormack@outlook.com>
2024-11-12 10:38:24 +0000

    Create MainMenuGUI.java

---
+++ Eclipse Program/src/Newsagent/MainMenuGUI.java
@@ -0,0 +1,11 @@
+package Newsagent;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainMenuGUI extends JFrame
+{
+	
+}
